9fadf1254d59bd976d2054230125fe5a8faee0e3
ueda66 <y6u6k.u@gmail.com>
2017-01-17 22:51:15 +0900

    再提出

---
+++ 004.JSPとサーブレット２/introduce.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yoko;
+
+/**
+ *
+ * @author yoko
+ */
+public class introduce {
+    public static void main(String[] args){
+     for(int i=0;i<10;i++){
+      name();
+      birth();
+      from();
+     }
+   }
+public static void name(){
+ System.out.println("上田容子");
+}
+public static void birth(){
+ System.out.println("1991年6月6日生まれ");
+}
+public static void from(){
+ System.out.println("宮城県出身");
+}
+
+    
+}
